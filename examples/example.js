b81e22f3b560722beb27526faf3a1455086e1081
Jack Wu <jackchased@gmail.com>
2016-04-06 11:37:26 +0800

    v0.0.2

---
+++ examples/example.js
@@ -0,0 +1,34 @@
+var ccznp = require('../index.js');
+
+var spCfg = {
+    path: "/dev/ttyUSB0",
+    options: {
+        baudrate: 115200,
+        flowControl: true,
+        rtscts: true
+    }
+};
+
+ccznp.on('ready', function () {
+    ccznp.sysRequest('ping', {}, function (err, result) {
+        if (err)
+            console.log(err);
+        else
+            console.log(result);
+    });
+
+    ccznp.sysRequest('version', {}, function (err, result) {
+        if (err)
+            console.log(err);
+        else
+            console.log(result);
+    });
+});
+
+ccznp.on('AREQ', function(result) {
+    console.log(result);
+});
+
+ccznp.init(spCfg, function (err) {
+    console.log(err);
+});
